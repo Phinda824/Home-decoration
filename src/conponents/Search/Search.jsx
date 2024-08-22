58c6a2d20ef1b7cdd115079dea624e4727ec3a1c
Phinda Simanga <simanga824@gmail.com>
2024-08-22 16:01:48 +0200

    updating

--- src/conponents/Search/Search.jsx
+++ src/conponents/Search/Search.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
+import products from "../../assets/data/products";
+import ProductsList from "../UI/ProductList";
+
 const Search = () => {
+  const [productData, setProductData] = useState([]);
   return (
     <div className="search_container">
       <input type="text" placeholder="Search Furniture" />
-      <div className="results"></div>
+      <div className="search_results">
+        <ProductsList data={products} />
+      </div>
     </div>
   );
 };
