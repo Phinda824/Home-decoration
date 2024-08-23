a468bb0f015253f45cfa9fbd76179d73935e7ebf
Phinda Simanga <simanga824@gmail.com>
2024-08-23 12:05:33 +0200

    updates

--- src/conponents/Search/SearchProduct.jsx
+++ src/conponents/Search/SearchProduct.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SearchProduct.css";
 
 import products from "../../assets/data/products";
 
@@ -6,8 +7,12 @@ const SearchProduct = () => {
   return (
     <div className="searchProduct_container">
       {products.map((item, index) => (
-        <div className="search_img" key={index}>
+        <div className="search_section" key={index}>
           <img src={item.imgUrl} alt="" />
+          <div className="item_section">
+            <h1>{item.productName}</h1>
+            <p>R {item.price}</p>
+          </div>
         </div>
       ))}
     </div>
